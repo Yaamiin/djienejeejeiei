8e4dbe20eeb34659c881f095a52af4b858c66d95
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-10-02 12:45:06 +0530

    Update admins.py

--- zaidmusic/admins.py
+++ zaidmusic/admins.py
@@ -15,7 +15,7 @@ from callsmusic import callsmusic
 from callsmusic.queues import queues
 from config import BOT_USERNAME, COMMAND_PREFIXES, que
 from cache.admins import admins
-from handlers.play import cb_admin_check
+from zaidmusic.play import cb_admin_check
 from helpers.channelmusic import get_chat_id
 from helpers.dbtools import delcmd_is_on, delcmd_off, delcmd_on, handle_user_status
 from helpers.decorators import authorized_users_only, errors
