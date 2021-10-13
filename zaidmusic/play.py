6d387d9c80106e15f89336dc7ffd2e3e4251b2b5
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-10-13 17:36:59 +0530

    Update play.py

--- zaidmusic/play.py
+++ zaidmusic/play.py
@@ -31,9 +31,15 @@ from config import (
 from downloaders import youtube
 from helpers.admins import get_administrators
 from helpers.channelmusic import get_chat_id
+from helpers.chattitle import CHAT_TITLE
 from helpers.decorators import authorized_users_only
 from helpers.filters import command, other_filters
 from helpers.gets import get_file_name
+from PIL import Image, ImageDraw, ImageFont
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtube_search import YoutubeSearch
 
 aiohttpsession = aiohttp.ClientSession()
 chat_id = None
