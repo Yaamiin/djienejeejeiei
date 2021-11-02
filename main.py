c379092071830a7e27e7e769f655e28183c671a3
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-11-02 22:10:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-import requests
-from pyrogram import Client as Bot
+import asyncio
 
-from callsmusic import run
-from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-
-response = requests.get(BG_IMAGE)
-with open("./etc/foreground.png", "wb") as file:
-    file.write(response.content)
+from program import BOT_ID, USERBOT_ID
+from driver.veez import call_py, bot, user
+from pytgcalls import idle
 
 
-bot = Bot(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="zaidmusic"),
-)
+async def all_info(bot, user):
+    global BOT_ID, USERBOT_ID
+    getme = await bot.get_me()
+    getme1 = await user.get_me()
+    BOT_ID = getme.id
+    USERBOT_ID = getme1.id
 
-print("[INFO]: ZAIDMUSIC STARTED!")
 
-bot.start()
-run()
+async def mulai_bot():
+    print("[INFO]: STARTING BOT CLIENT")
+    await bot.start()
+    print("[INFO]: STARTING PYTGCALLS CLIENT")
+    await call_py.start()
+    print("[INFO]: GENERATING CLIENT PROFILE")
+    await all_info(bot, user)
+    await idle()
+    print("[INFO]: STOPPING BOT")
+    await bot.stop()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(mulai_bot())
