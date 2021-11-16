cbdf988ab1c5dabd75842e44e9fc4c37cd404c6e
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-11-16 14:32:17 +0530

    Add files via upload
    updates and some fixes

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -1,32 +1,92 @@
+from typing import Dict
+
+from pytgcalls import GroupCall
+
 from . import queues
-from pyrogram import Client
-from pytgcalls import PyTgCalls
-from pytgcalls.types import Update
-from pytgcalls.types.input_stream import InputAudioStream
-from pytgcalls.types.input_stream import InputStream
-from config import API_HASH, API_ID, SESSION_NAME
+from . import client
+
+instances: Dict[int, GroupCall] = {}
+active_chats: Dict[int, Dict[str, bool]] = {}
 
 
-client = Client(SESSION_NAME, API_ID, API_HASH)
-pytgcalls = PyTgCalls(client)
+def init_instance(chat_id: int):
+    if chat_id not in instances:
+        instances[chat_id] = GroupCall(client)
+
+    instance = instances[chat_id]
+
+    @instance.on_playout_ended
+    async def ___(__, _):
+        queues.task_done(chat_id)
+
+        if queues.is_empty(chat_id):
+            await stop(chat_id)
+        else:
+            instance.input_filename = queues.get(chat_id)["file"]
 
 
-@pytgcalls.on_stream_end()
-async def on_stream_end(client: PyTgCalls, update: Update) -> None:
-    chat_id = update.chat_id
-    queues.task_done(chat_id)
-
-    if queues.is_empty(chat_id):
-        await pytgcalls.leave_group_call(chat_id)
-    else:
-        await pytgcalls.change_stream(
-            chat_id, 
-            InputStream(
-                InputAudioStream(
-                    queues.get(chat_id)["file"],
-                ),
-            ),
-        )
+def get_instance(chat_id: int) -> GroupCall:
+    init_instance(chat_id)
+    return instances[chat_id]
 
 
-run = pytgcalls.start
+async def start(chat_id: int):
+    await get_instance(chat_id).start(chat_id)
+    active_chats[chat_id] = {"playing": True, "muted": False}
+
+
+async def stop(chat_id: int):
+    await get_instance(chat_id).stop()
+
+    if chat_id in active_chats:
+        del active_chats[chat_id]
+
+
+async def set_stream(chat_id: int, file: str):
+    if chat_id not in active_chats:
+        await start(chat_id)
+    get_instance(chat_id).input_filename = file
+
+
+def pause(chat_id: int) -> bool:
+    if chat_id not in active_chats:
+        return False
+    elif not active_chats[chat_id]["playing"]:
+        return False
+
+    get_instance(chat_id).pause_playout()
+    active_chats[chat_id]["playing"] = False
+    return True
+
+
+def resume(chat_id: int) -> bool:
+    if chat_id not in active_chats:
+        return False
+    elif active_chats[chat_id]["playing"]:
+        return False
+
+    get_instance(chat_id).resume_playout()
+    active_chats[chat_id]["playing"] = True
+    return True
+
+
+def mute(chat_id: int) -> int:
+    if chat_id not in active_chats:
+        return 2
+    elif active_chats[chat_id]["muted"]:
+        return 1
+
+    get_instance(chat_id).set_is_mute(True)
+    active_chats[chat_id]["muted"] = True
+    return 0
+
+
+def unmute(chat_id: int) -> int:
+    if chat_id not in active_chats:
+        return 2
+    elif not active_chats[chat_id]["muted"]:
+        return 1
+
+    get_instance(chat_id).set_is_mute(False)
+    active_chats[chat_id]["muted"] = False
+    return 0
