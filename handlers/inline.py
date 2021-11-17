ff3b7a4a34a943c5ef5a415b2809ccf0069a534a
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-11-17 21:11:59 +0530

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -1,7 +1,11 @@
+import json
 from pyrogram import Client, errors
-from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
-
-from youtubesearchpython import VideosSearch
+from pyrogram.types import (
+    InlineQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+)
+from youtube_search import YoutubeSearch
 
 
 @Client.on_inline_query()
@@ -13,39 +17,33 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Type a YouTube video name...",
+            switch_pm_text="type a youtube video name...",
             switch_pm_parameter="help",
-            cache_time=0
+            cache_time=0,
         )
     else:
-        search = VideosSearch(search_query, limit=50)
-
-        for result in search.result()["result"]:
-            answers.append(
-                InlineQueryResultArticle(
-                    title=result["title"],
-                    description="{}, {} views.".format(
-                        result["duration"],
-                        result["viewCount"]["short"]
-                    ),
-                    input_message_content=InputTextMessageContent(
-                        "https://www.youtube.com/watch?v={}".format(
-                            result["id"]
-                        )
-                    ),
-                    thumb_url=result["thumbnails"][0]["url"]
-                )
+        results = YoutubeSearch(search_query, limit=10).to_dict()
+        
+        answers.append(
+            InlineQueryResultArticle(
+                title=result["title"],
+                description="{}, {} views.".format(
+                    result["duration"], result["viewCount"]["short"]
+                ),
+                input_message_content=InputTextMessageContent(
+                    "🔗 https://www.youtube.com/watch?v={}".format(result["id"])
+                ),
+                thumb_url=result["thumbnails"][0]["url"],
             )
-
+        )
+        
+        
         try:
-            await query.answer(
-                results=answers,
-                cache_time=0
-            )
+            await query.answer(results=answers, cache_time=0)
         except errors.QueryIdInvalid:
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="Error: Search timed out",
+                switch_pm_text="Error: search timed out",
                 switch_pm_parameter="",
             )
